b0d108df1a83d72639b78c9eeb8ac6647e0a2989
Jeffrey Luo <114297879+JeffreyLuo333@users.noreply.github.com>
2024-06-15 19:31:27 -0700

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,43 @@
+
+import gradio as gr
+
+def greet(prompt):
+        return "Clutch.mp4","Whiff.mp4" #Use other mp4s
+
+
+with gr.Blocks(title = "Videobot Arena") as demo:
+    with gr.Tab("Arena (battle)"):
+        with gr.Row():
+            gr.Markdown(
+            """
+            # Videobot Arena
+            Start typing below to compare models
+            """)
+        with gr.Row():
+            title="Videobot Arena"
+            output1=gr.Video(label="Model A")
+            output2=gr.Video(label="Model B")
+        with gr.Row():
+            abetter_btn = gr.Button("<- A is better")
+            bbetter_btn = gr.Button("B is better ->")
+            tie_btn = gr.Button("Both are good")
+            both_btn = gr.Button("Both are bad")
+        with gr.Row():
+            text = gr.Textbox(label="Enter your prompt and hit ENTER", min_width=1100)
+            submit_btn = gr.Button("Submit")
+            text.submit(fn=greet, inputs = text, outputs = [output1,output2], scroll_to_output=True)
+            submit_btn.click(fn=greet, inputs = text, outputs = [output1,output2], scroll_to_output=True)
+        with gr.Row():
+            clear_btn = gr.ClearButton(text)
+            regenerate_btn = gr.Button("Regenerate")
+    with gr.Tab("Arena (1v1)"):
+        gr.Markdown("# Videobot Arena: 1v1")
+    with gr.Tab("Single Model"):
+        gr.Markdown("# Single Model")
+    with gr.Tab("Leaderboards"):
+        gr.Markdown("# Videobot Arena Leaderboard")
+    with gr.Tab("About Us"):
+        gr.Markdown("# About Us")
+
+
+demo.launch(share=True)
