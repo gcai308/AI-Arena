3410196cb7ce2599ebeb3dba429dca2b981cd1dc
Garrett Cai <garrett.y.cai@gmail.com>
2024-06-19 10:33:31 -0700

    display data

--- src/data.py
+++ src/data.py
@@ -16,4 +16,18 @@ def add_entry(prompt, model_a, model_b, vote):
             sql = "INSERT INTO `votes` (`time`, `prompt`,  `model_a`,  `model_b`,  `vote`) VALUES (%s, %s, %s, %s, %s)"
             cursor.execute(sql, (datetime.now(), prompt, model_a, model_b, vote))
         connection.commit()
+
+
+def vote_data():
+    connection = pymysql.connect(host = 'localhost',
+                                 user = 'root',
+                                 password = 'Puresug@r',
+                                 database = 'vote_data',
+                                 charset = 'utf8mb4',
+                                 cursorclass = pymysql.cursors.Cursor)
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `votes`"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
     
--- src/leaderboard.py
+++ src/leaderboard.py
@@ -1,5 +1,29 @@
 import gradio as gr
+import data
 
 def build_tab():
     with gr.Tab("Leaderboards"):
         gr.Markdown("# Videobot Arena Leaderboard")
+        df = gr.Dataframe(
+            headers=[
+                "Num",
+                "Time",
+                "Prompt",
+                "Model A",
+                "Model B",
+                "Vote",
+            ],
+            datatype=[
+                "number",
+                "date",
+                "str",
+                "str",
+                "str",
+                "number"
+            ],
+            height=700,
+            column_widths=[50,200,250,150,150,100],   
+            wrap=True,
+        )
+    data.vote_data()
+        
