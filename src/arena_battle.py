9c902af739aaa1a816b8ad50c60a2cd6b17d4fd5
Garrett Cai <garrett.y.cai@gmail.com>
2024-06-18 23:02:15 -0700

    Update arena_battle.py

--- src/arena_battle.py
+++ src/arena_battle.py
@@ -2,17 +2,20 @@ import gradio as gr
 import data
 
 current = ''
-abetter = 0
-bbetter = 1
-tie = 2
-both = 3
 
 def generate(prompt):
     current = prompt
     return "Clutch.mp4","Whiff.mp4" #Use other mp4s
 
-def vote(vote):
-    data.add_entry(current, 'test', 'test', vote)
+def a_vote():
+    data.add_entry(current, 'test', 'test', 0)
+def b_vote():
+    data.add_entry(current, 'test', 'test', 1)
+def tie_vote():
+    data.add_entry(current, 'test', 'test', 2)
+def both_vote():
+    data.add_entry(current, 'test', 'test', 3)
+    
     
 def build_tab():
     with gr.Tab("Arena (battle)"):
@@ -27,13 +30,13 @@ def build_tab():
             output2=gr.Video(label="Model B")
         with gr.Row():
             abetter_btn = gr.Button("<- A is better")
-            abetter_btn.click(fn=vote, inputs = abetter, outputs = [])
+            abetter_btn.click(fn=a_vote, inputs = [], outputs = [])
             bbetter_btn = gr.Button("B is better ->")
-            bbetter_btn.click(fn=vote, inputs = bbetter, outputs = [])
+            bbetter_btn.click(fn=b_vote, inputs = [], outputs = [])
             tie_btn = gr.Button("Tie")
-            tie_btn.click(fn=vote, inputs = tie, outputs = [])
+            tie_btn.click(fn=tie_vote, inputs = [], outputs = [])
             both_btn = gr.Button("Skip")
-            both_btn.click(fn=vote, inputs = both, outputs = [])
+            both_btn.click(fn=both_vote, inputs = [], outputs = [])
         with gr.Row():
             text = gr.Textbox(label="Enter your prompt and hit ENTER", min_width=1100)
             submit_btn = gr.Button("Submit")
