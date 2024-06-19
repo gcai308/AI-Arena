dcc4621a4a710b2997ab57d7590bb3f82d55fd37
Garrett Cai <garrett.y.cai@gmail.com>
2024-06-18 22:16:01 -0700

    database test 👎

--- src/arena_battle.py
+++ src/arena_battle.py
@@ -1,7 +1,18 @@
 import gradio as gr
+import data
+
+current = ''
+abetter = 0
+bbetter = 1
+tie = 2
+both = 3
 
 def generate(prompt):
-        return "Clutch.mp4","Whiff.mp4" #Use other mp4s
+    current = prompt
+    return "Clutch.mp4","Whiff.mp4" #Use other mp4s
+
+def vote(vote):
+    data.add_entry(current, 'test', 'test', vote)
     
 def build_tab():
     with gr.Tab("Arena (battle)"):
@@ -16,9 +27,13 @@ def build_tab():
             output2=gr.Video(label="Model B")
         with gr.Row():
             abetter_btn = gr.Button("<- A is better")
+            abetter_btn.click(fn=vote, inputs = abetter, outputs = [])
             bbetter_btn = gr.Button("B is better ->")
+            bbetter_btn.click(fn=vote, inputs = bbetter, outputs = [])
             tie_btn = gr.Button("Tie")
+            tie_btn.click(fn=vote, inputs = tie, outputs = [])
             both_btn = gr.Button("Skip")
+            both_btn.click(fn=vote, inputs = both, outputs = [])
         with gr.Row():
             text = gr.Textbox(label="Enter your prompt and hit ENTER", min_width=1100)
             submit_btn = gr.Button("Submit")
