272c3cb758a05a62a0e4fc5e49b248767a134d3f
root <garrett.y.cai@gmail.com>
2024-06-19 06:25:03 +0000

    voting works now

--- src/data.py
+++ src/data.py
@@ -2,17 +2,15 @@ import gradio as gr
 import pymysql
 from datetime import datetime
 
-connection = pymysql.connect(host = 'localhost',
-                             user = 'root',
-                             password = 'Puresug@r',
-                             database = 'vote_data',
-                             charset = 'utf8mb4',
-                             cursorclass = pymysql.cursors.Cursor)
-
-def conn():
-    return connection
 
 def add_entry(prompt, model_a, model_b, vote):
+    connection = pymysql.connect(host = 'localhost',
+                                 user = 'root',
+                                 password = 'Puresug@r',
+                                 database = 'vote_data',
+                                 charset = 'utf8mb4',
+                                 cursorclass = pymysql.cursors.Cursor)
+
     with connection:
         with connection.cursor() as cursor:
             sql = "INSERT INTO `votes` (`time`, `prompt`,  `model_a`,  `model_b`,  `vote`) VALUES (%s, %s, %s, %s, %s)"
